8adaf83451b6a6571a24bd3204a4bb810f1149c2
kyungmin kim <ee5201@naver.com>
2023-05-23 00:07:17 +0900

    updateStudy

---
+++ src/components/units/board/09-write/boardwrite.container.js
@@ -0,0 +1,66 @@
+import { useState } from "react"
+import BoardWriterPresenter from "./boardwrite.presenter"
+import { CREATEBOARD, UPDATEBOARD } from "./boardwrite.queries"
+import { useMutation } from "@apollo/client"
+import { useRouter } from "next/router"
+
+export default function BoardWriterContainer (props) {
+  const [writer,setWriter] = useState("")
+  const [title,settitle] = useState("")
+  const [contents,setContent] = useState("")
+  const [CreateBoard] = useMutation(CREATEBOARD)
+  const [updateBoard]= useMutation(UPDATEBOARD)
+  const router = useRouter()
+
+
+  const onchangeWriter = (event) =>{
+    setWriter(event.target.value)
+  }
+  const onCHangetitle = (event) =>{
+    settitle(event.target.value)
+  }
+  const onChangecontents = (event) =>{
+    setContent(event.target.value)
+  }
+  const onClickSubmit = async() =>{
+    const result = await CreateBoard({
+      variables:{
+        writer:writer,
+        title:title,
+        contents:contents
+      }
+    })
+    console.log(result)
+    router.push(`/section09/09-03-boards/${result.data.createBoard.number}`)
+
+
+  }
+
+  const onClickupdate = async() =>{
+    const myvariables = {number:Number(router.query.number)}
+    if(writer) myvariables.writer = writer
+    if(title) myvariables.title = title
+    if(contents) myvariables.contents = contents
+    const result = await updateBoard({
+      variables:myvariables
+      
+    })
+    router.push(`/section09/09-03-boards/${result.data.updateBoard.number}`)
+    
+  }
+
+
+  return(
+    <>
+    <BoardWriterPresenter
+    onchangeWriter={onchangeWriter}
+    onCHangetitle={onCHangetitle}
+    onChangecontents={onChangecontents}
+    onClickSubmit={onClickSubmit}
+    isEdit={props.isEdit}
+    onClickupdate={onClickupdate}
+    data={props.data}
+    />
+    </>
+  )
+}
---
+++ src/components/units/board/09-write/boardwrite.presenter.js
@@ -0,0 +1,18 @@
+import { gql, useQuery } from "@apollo/client"
+import { useRouter } from "next/router"
+
+
+export default function BoardWriterPresenter ({onchangeWriter,onCHangetitle,onChangecontents,onClickSubmit,isEdit,onClickupdate,data}) {
+
+
+  return(
+  <>
+    작성자:<input type="text" onChange={onchangeWriter} defaultValue={data?.fetchBoard?.writer}/>
+      제목: <input type="text" onChange={onCHangetitle}
+      defaultValue={data?.fetchBoard?.title }/>
+      내용:<input type="text" onChange={onChangecontents}
+      defaultValue={data?.fetchBoard?.contents }/>
+      <button onClick={isEdit ? onClickupdate :onClickSubmit}>{isEdit ? "수정" : "등록"}하기</button>
+  </>
+  )
+}
